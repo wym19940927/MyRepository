cb1b9016600f4ad11e5d0cfa9b8b5ce09606abab
wym <wym19940927@163.com>
2020-08-06 18:18:18 +0800

    second commit

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-// import BaiduMap from 'vue-baidu-map'
-// Vue.use(BaiduMap, {
-//   ak: '你申请的key'
-// })
-
 Vue.config.productionTip = false;
 
 new Vue({
