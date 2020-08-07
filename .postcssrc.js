4b0a9dee69d44e78395680bee9664c40151ebb33
wym <wym19940927@163.com>
2020-08-07 16:03:14 +0800

    修改容器大小

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,29 +1,25 @@
 module.exports = {
-    "plugins": {
-        "postcss-import": {},
-        "postcss-url": {},
-        "autoprefixer": {},
-        "postcss-write-svg": {
-          "utf8": false
-        },
-        "postcss-cssnext": {},
-        "postcss-px-to-viewport": {
-          "viewportWidth": 750,
-          "unitPrecision": 3,
-          "viewportUnit": "vw",
-          "selectorBlackList": [
-            ".ignore",
-            ".hairlines"
-          ],
-          "minPixelValue": 1,
-          "mediaQuery": false,
-          "exclude": /(\/|\\)(node_modules)(\/|\\)/
-        },
-        "postcss-viewport-units": {},
-        "cssnano": {
-          "preset": "advanced",
-          "autoprefixer": false,
-          "postcss-zindex": false
-        }
-    }
-}
+  plugins: {
+    "postcss-import": {},
+    "postcss-url": {},
+    autoprefixer: {},
+    "postcss-write-svg": {
+      utf8: false,
+    },
+    "postcss-px-to-viewport": {
+      viewportWidth: 750,
+      unitPrecision: 3,
+      viewportUnit: "vw",
+      selectorBlackList: [".ignore", ".hairlines"],
+      minPixelValue: 1,
+      mediaQuery: false,
+      exclude: /(\/|\\)(node_modules)(\/|\\)/,
+    },
+    "postcss-viewport-units": {},
+    cssnano: {
+      preset: "advanced",
+      autoprefixer: false,
+      "postcss-zindex": false,
+    },
+  },
+};
--- src/components/home/HabitList.vue
+++ src/components/home/HabitList.vue
@@ -1,24 +1,19 @@
 <template>
-  <div id="habit_list">
-     
-  </div>
+  <div id="habit_list"></div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class HabitList extends Vue {
-    
-}
-
+export default class HabitList extends Vue {}
 </script>
 <style lang="scss">
-    #habit_list {
-        position: relative;
-        padding-top: 200px;
-        padding-bottom: 100px;
-        height: 3000px;
-        background-color: #f1f1f1;
-    }
+#habit_list {
+  position: relative;
+  padding-top: 200px;
+  padding-bottom: 100px;
+  height: 100%;
+  box-sizing: border-box;
+}
 </style>
